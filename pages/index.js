2095c74af823d8f5dbd916dca600345888ba9be9
Rubén Sahagún <rubenofen@gmail.com>
2022-10-18 09:46:04 +0200

    bad deploy

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,6 @@ export default function Home() {
         <p className={styles.description}>
           Get started by editing{" "}
           <code className={styles.code}>pages/index.js</code>
-        </p>
 
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
